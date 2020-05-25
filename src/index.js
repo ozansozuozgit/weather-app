466d2605391cbf66524dbfa77aec7cd1350a34dc
Ozan Sozuoz <ozansozuoz@hotmail.com>
2020-05-25 22:25:34 +0300

    cleaned up code

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import 'normalize.css';
-import './styles/style.css';
-import './eventListeners';
-// import all images
-const reqSvgs = require.context('./images', true, /\.svg$/);
-
-console.log('JS working');
